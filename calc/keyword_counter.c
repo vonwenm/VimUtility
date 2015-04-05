29704c9e61a9770eaf5ed14c000a89a3b48a5f9a
Mrvon <mrvon@qq.com>
2015-04-05 21:36:35 +0800

    small change

--- calc/keyword_counter.c
+++ calc/keyword_counter.c
@@ -19,11 +19,36 @@ key keytab[] = {
     {"const", 0},
     {"continue", 0},
     {"default", 0},
+    {"do", 0},
+    {"double", 0},
+    {"else", 0},
+    {"enum", 0},
+    {"extern", 0},
+    {"float", 0},
+    {"for", 0},
+    {"goto", 0},
+    {"if", 0},
+    {"int", 0},
+    {"long", 0},
+    {"register", 0},
+    {"return", 0},
+    {"short", 0},
+    {"signed", 0},
+    {"sizeof", 0},
+    {"static", 0},
+    {"struct", 0},
+    {"switch", 0},
+    {"typedef", 0},
+    {"union", 0},
+    {"unsigned", 0},
+    {"void", 0},
+    {"volatile", 0},
+    {"while", 0},
 };
 
 #define NKEYS (sizeof(keytab) / sizeof(key))
 
-int binserach(char*, key*, int);
+int binsearch(char*, key*, int);
 
 int main(int argc, char const* argv[])
 {
@@ -32,7 +57,7 @@ int main(int argc, char const* argv[])
 
     while (getword(word, MAXWORD) != EOF) {
         if (isalpha(word[0])) {
-            if ((n = binserach(word, keytab, NKEYS)) >= 0) {
+            if ((n = binsearch(word, keytab, NKEYS)) >= 0) {
                 keytab[n].count++;
             }
         }
@@ -46,7 +71,7 @@ int main(int argc, char const* argv[])
     return 0;
 }
 
-int binserach(char* word, key tab[], int n)
+int binsearch(char* word, key tab[], int n)
 {
     int cond;
     int low;
