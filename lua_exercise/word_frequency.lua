795dfdcdc3301a1768023d938b6917fc38fd82d6
Dennis <mrvon@qq.com>
2015-05-07 17:00:52 +0800

    fix word_frequency

--- lua_exercise/word_frequency.lua
+++ lua_exercise/word_frequency.lua
@@ -1,5 +1,38 @@
 local WORD_MIN_LEN = 4
 
+local function ignore_by_length(w)
+    if #w >= WORD_MIN_LEN then
+        return false
+    else
+        return true
+    end
+end
+
+local function make_ignore_dict()
+    local f = assert(io.open("ignore.txt", "r"))
+    local text = f.read(f, "*a")
+    if text == nil then
+        print("read dict file failed")
+        return
+    end
+
+    local dict = {}
+    for word in string.gmatch(text, "%w+") do
+        dict[word] = true
+    end
+    return dict
+end
+
+local ignore_dict = make_ignore_dict()
+
+local function ignore_by_dict(w, dict)
+    if dict[w] then
+        return true
+    else
+        return false
+    end
+end
+
 local function allwords()
     local auxwords = function()
         for line in io.lines() do
@@ -13,7 +46,7 @@ end
 
 local counter = {}
 for w in allwords() do
-    if #w >= WORD_MIN_LEN then
+    if not ignore_by_dict(w, ignore_dict) then
         counter[w] = (counter[w] or 0) + 1
     end
 end
