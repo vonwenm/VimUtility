dd8fa302a980a1f33d34f392eef4c20a80204bbc
Dennis <mrvon@qq.com>
2015-05-08 12:02:39 +0800

    breadth first find path

--- lua_exercise/dijkstra.lua
+++ lua_exercise/dijkstra.lua
@@ -66,19 +66,55 @@ local function dijkstra(g, s, w)
         end
     end
 
-    printpath(P, w)
+    printpath(P, w, D)
 end
 
-function printpath(P, w)
+local function breadth_first_find_path(g, s, w)
+    local visited = {}
+    local Q = {}
+    local D = {}
+    local P = {}
+    table.insert(Q, s)
+
+    local i = 1
+    for _, node in pairs(g) do
+        D[node] = math.huge
+        i = i + 1
+    end
+    D[s] = 0
+
+    while #Q > 0 do
+        local u = Q[1]
+        table.remove(Q, 1)
+
+        for v, d in pairs(u.adj) do
+            if D[v] > D[u] + d then
+                D[v] = D[u] + d
+                P[v] = u
+            end
+
+            if not visited[v] then
+                visited[v] = true
+                table.insert(Q, v)
+            end
+        end
+    end
+
+    printpath(P, w, D)
+end
+
+function printpath(P, w, D)
+    print("path length: " .. D[w])
     while P[w] do
         io.write(w.name .. " ")
         w = P[w]
     end
-    io.write(w.name .. "\n")
+    print(w.name)
 end
 
 g = readgraph()
 s = name2node(g, "a")
 w = name2node(g, "b")
-dijkstra(g, s, w)
 
+dijkstra(g, s, w)
+breadth_first_find_path(g, s, w)
--- lua_exercise/graph.lua
+++ lua_exercise/graph.lua
@@ -12,18 +12,18 @@ local function readgraph()
     local graph = {}
     for line in io.lines() do
         -- split line in two names
-        local name_from, name_to, arc_label = string.match(line, "(%S+)%s+(%S+)%s+(%d+)")
+        local name_from, name_to, arc_length = string.match(line, "(%S+)%s+(%S+)%s+(%d+)")
         -- find corresponding nodes
         local from = name2node(graph, name_from)
         local to = name2node(graph, name_to)
         -- adds 'to' to the adjacent set of 'from'
-        from.adj[to] = arc_label
+        from.adj[to] = arc_length
     end
     return graph
 end
 
 -- deep first search
-local function findpath(curr, to, path, visited)
+local function depth_first_find_path(curr, to, path, visited)
     path = path or {}
     visited = visited or {}
     if visited[curr] then  -- node already visited
@@ -36,7 +36,7 @@ local function findpath(curr, to, path, visited)
     end
     -- try all adjacent nodes
     for node in pairs(curr.adj) do
-        local p = findpath(node, to, path, visited)
+        local p = depth_first_find_path(node, to, path, visited)
         if p then
             return p
         end
@@ -64,5 +64,5 @@ end
 g = readgraph()
 a = name2node(g, "a")
 b = name2node(g, "b")
-p = findpath(a, b)
+p = depth_first_find_path(a, b)
 printpath(p)
