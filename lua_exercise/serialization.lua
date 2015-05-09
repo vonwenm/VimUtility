ef6ed29e3d8b5c24cbd0513808e40836ee61d345
Mrvon <mrvon@qq.com>
2015-05-09 12:59:43 +0800

    fix serialization

--- lua_exercise/serialization.lua
+++ lua_exercise/serialization.lua
@@ -1,3 +1,29 @@
+function mywrite(out_arr, ...)
+    local t = {...}
+    for i = 1, #t do
+        table.insert(out_arr, t[i])
+    end
+end
+
+function isvalidindentfier(k)
+    local pattern = "[_%a][_%a%d]*"
+    local str = string.match(k, pattern)
+    if k == str then
+        return true
+    else
+        return false
+    end
+end
+assert(isvalidindentfier("a"))
+assert(isvalidindentfier("a1"))
+assert(isvalidindentfier("_a1"))
+assert(isvalidindentfier("__a1"))
+assert(isvalidindentfier("__1"))
+assert(isvalidindentfier("__abc"))
+assert(isvalidindentfier("__ab1c3"))
+assert(not isvalidindentfier("1a"))
+assert(not isvalidindentfier("$a"))
+
 function quote(s)
     -- find maximum length of sequence of equal signs
     local n = -1
@@ -11,33 +37,53 @@ function quote(s)
     -- build quoted string
     return string.format("[%s[%s]%s]", eq, s, eq)
 end
+-- print(quote("[===[hello world [[Nest]] ]===]"))
+
+function indent(out_arr, indent_level)
+    for i = 1, indent_level do
+        mywrite(out_arr, "\t")
+    end
+end
 
 function serialize(o)
+    local out_arr = {}
+    aux_serialize(out_arr, o)
+    return table.concat(out_arr)
+end
+
+function aux_serialize(out_arr, o, indent_level)
+    indent_level = indent_level or 0
     if type(o) == "number" then
-        io.write(o)
+        mywrite(out_arr, o)
     elseif type(o) == "string" then
-        io.write(string.format("%q", o))
+        mywrite(out_arr, string.format("%q", o))
     elseif type(o) == "table" then
-        io.write("{\n")
+        mywrite(out_arr, "{\n")
+        indent(out_arr, indent_level)
         for k, v in pairs(o) do
-            io.write("[")
-            serialize(k)
-            io.write("] = ")
-            serialize(v)
-            io.write(",\n")
+            if not isvalidindentfier(k) then
+                mywrite(out_arr, "  [")
+                aux_serialize(out_arr, k, indent_level + 1)
+                mywrite(out_arr, "] = ")
+            else
+                mywrite(out_arr, "  " .. k .. " = ")
+            end
+            aux_serialize(out_arr, v, indent_level + 1)
+            mywrite(out_arr, ",\n")
+            indent(out_arr, indent_level)
         end
-        io.write("}\n")
+        mywrite(out_arr, "}\n")
+        indent(out_arr, indent_level)
     else
         error("cannot serialize a " .. type(o))
     end
 end
 
--- print(quote("[===[hello world [[Nest]] ]===]"))
 
--- serialize({a = 12, b = 'lua', key = 'another "one"'})
--- serialize({[1] = 12, [2] = 'lua', [3] = 'another "one"'})
--- serialize({["1a"] = 12, ["1b"] = 'lua', ["1c"] = 'another "one"'})
--- serialize({["1a"] = {"Nest String", {"Nest String 2"}}, ["1b"] = 'lua', ["1c"] = 'another "one"'})
+print(serialize({a = 12, b = 'lua', key = 'another "one"', "hello world"}))
+print(serialize({[1] = 12, [2] = 'lua', [3] = 'another "one"'}))
+print(serialize({["1a"] = 12, ["1b"] = 'lua', ["1c"] = 'another "one"'}))
+print(serialize({["1a"] = {nest_str = "Hello world", {nest_str = "Hi!"}}, ["1b"] = 'lua', ["1c"] = 'another "one"'}))
 
 function basic_serialize(o)
     if type(o) == "number" then
@@ -50,20 +96,26 @@ function basic_serialize(o)
 end
 
 function save(name, value, saved)
+    local out_arr = {}
+    aux_save(out_arr, name, value, saved)
+    return table.concat(out_arr)
+end
+
+function aux_save(out_arr, name, value, saved)
     saved = saved or {}                                        -- initial value
-    io.write(name, " = ")
+    mywrite(out_arr, name, " = ")
     if type(value) == "number" or type(value) == "string" then
-        io.write(basic_serialize(value), "\n")
+        mywrite(out_arr, basic_serialize(value), "\n")
     elseif type(value) == "table" then
         if saved[value] then                                   -- value already saved?
-            io.write(saved[value], "\n")                       -- use its previous name
+            mywrite(out_arr, saved[value], "\n")                       -- use its previous name
         else
             saved[value] = name                                -- save name for next time
-            io.write("{}\n")                                   -- create a new table
+            mywrite(out_arr, "{}\n")                                   -- create a new table
             for k, v in pairs(value) do                        -- save its fields
                 k = basic_serialize(k)
                 local fname = string.format("%s[%s]", name, k)
-                save(fname, v, saved)
+                aux_save(out_arr, fname, v, saved)
             end
         end
     else
@@ -71,8 +123,9 @@ function save(name, value, saved)
     end
 end
 
-local t = {}
-a = {{"one", "two"}, 3}
-b = {k = a[1]}
-save("a", a, t)
-save("b", b, t)
+-- local t = {}
+-- a = {{"one", "two"}, 3}
+-- b = {k = a[1]}
+-- print(save("a", a, t))
+-- print(save("b", b, t))
+
