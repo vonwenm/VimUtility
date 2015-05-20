a5380e9e64c786b7f2333025821da5940ef8ba98
Dennis <mrvon@qq.com>
2015-05-20 17:59:21 +0800

    account

--- lua_exercise/account.lua
+++ lua_exercise/account.lua
@@ -34,8 +34,58 @@ function SpecialAccount:getLimit()
     return self.limit or 0
 end
 
-s = SpecialAccount:new({limit = 1000.00})
-s:deposit(100)
-s:detail()
-s:withdraw(900)
-s:detail()
+-- s = SpecialAccount:new({limit = 1000.00})
+-- s:deposit(100)
+-- s:detail()
+-- s:withdraw(900)
+-- s:detail()
+
+
+Named = {}
+
+function Named:getname()
+    return self.name
+end
+
+function Named:setname(n)
+    self.name = n
+end
+
+-- look up for 'k' in list of tables 'plist'
+local function search(k, plist)
+    for i = 1, #plist do
+        local v = plist[i][k]
+        if v then
+            return v
+        end
+    end
+end
+
+function createClass(...)
+    local c = {} -- new class
+    local parents = {...}
+
+    -- class with search for each method in the list of its parents
+    setmetatable(c, {
+        __index = function(t, k)
+            return search(k, parents)
+        end
+    })
+    
+    -- prepare 'c' to be the metatable of its instance
+    c.__index = c
+
+    -- define a new constructor for this new class
+    function c:new(o)
+        o = o or {}
+        setmetatable(o, c)
+        return o
+    end
+
+    return c -- return new class
+end
+
+NamedAccount = createClass(Account, Named)
+account = NamedAccount:new({name = "Paul"})
+print(account:getname())
+account:detail()
